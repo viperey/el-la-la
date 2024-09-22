d543d450f5c2fab040fed9a4365dcf9a6f600676
Viperey <viperey@riseup.net>
2024-09-22 09:10:30 +0200

    Moar FP style

--- src/repository/nouns.rs
+++ src/repository/nouns.rs
@@ -1,7 +1,7 @@
 use crate::domain::{Gender, Noun};
 use crate::repository::connector;
 use mysql::prelude::Queryable;
-use mysql::{params, PooledConn};
+use mysql::{params, Params, PooledConn};
 use std::error::Error;
 
 pub struct NounsRepository {
@@ -14,25 +14,16 @@ impl NounsRepository {
         NounsRepository { conn }
     }
 
-    fn build(
-        id: i32,
-        english: String,
-        spanish: String,
-        gender_str: String,
-    ) -> Result<Noun, Box<dyn Error>> {
+    fn build(result: Option<(i32, String, String, String)>) -> Result<Noun, Box<dyn Error>> {
+        let (id, english, spanish, gender_str) = result.ok_or("No nouns found in the database")?;
+
         let gender: Gender = match gender_str.as_str() {
             "masculine" => Gender::Masculine,
             "feminine" => Gender::Feminine,
             "any" => Gender::Any,
             _ => return Err(format!("Invalid gender '{}' in database", gender_str).into()),
         };
-
-        Ok(Noun {
-            id,
-            english,
-            spanish,
-            gender,
-        })
+        Ok(Noun {id, english, spanish, gender})
     }
 }
 
@@ -43,28 +34,22 @@ pub trait NounsRepositoryTrait {
 
 impl NounsRepositoryTrait for NounsRepository {
     fn get(&mut self, id: i32) -> Result<Noun, Box<dyn Error>> {
-        let result: Option<(i32, String, String, String)> = self.conn.exec_first(
-            "SELECT id, english, spanish, gender FROM nouns WHERE id = :id",
-            params! {
-                "id" => id,
-            },
-        )?;
-
-        if let Some((id, english, spanish, gender_str)) = result {
-            Self::build(id, english, spanish, gender_str)
-        } else {
-            Err(format!("Noun with id {} not found", id).into())
-        }
+        let statement: &str = "SELECT id, english, spanish, gender FROM nouns WHERE id = :id";
+        let params: Params = params! {"id" => id};
+        self.conn
+            .exec_first(statement, params)
+            .map_err(|e| e.into())
+            .and_then(Self::build)
     }
     fn get_random(&mut self) -> Result<Noun, Box<dyn Error>> {
-        let result: Option<(i32, String, String, String)> = self.conn.query_first(
-            "SELECT id, english, spanish, gender FROM nouns ORDER BY RAND() LIMIT 1",
-        )?;
-
-        if let Some((id, english, spanish, gender_str)) = result {
-            Self::build(id, english, spanish, gender_str)
-        } else {
-            Err("No nouns found in the database".into())
-        }
+        let statement: &str = "\
+            SELECT id, english, spanish, gender \
+            FROM nouns \
+            ORDER BY RAND() \
+            LIMIT 1";
+        self.conn
+            .query_first(statement)
+            .map_err(|e| e.into())
+            .and_then(Self::build)
     }
 }
